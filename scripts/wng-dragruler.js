f2fc196e1a65119c39055d2e3b84c65144329042
jmhysong <jmhysong@gmail.com>
2022-01-21 04:39:12 -0500

    Release 2.0.0
    Massive changes across the board, too many to list.
    
    #TODO Work on the documentation.
    #TODO Adjust the range colors for increased clarity.
    #TODO Account for other types of environments, abilities, and equipment.

--- scripts/wng-dragruler.js
+++ scripts/wng-dragruler.js
@@ -2,74 +2,138 @@ Hooks.once("dragRuler.ready", (SpeedProvider) => {
     class WnGSpeedProvider extends SpeedProvider {
         get colors() {
             return [
-                {id: "crawl", default: 0xFFFF00, name: "Crawl"},
-                {id: "standard", default: 0x00FF00, name: "Standard"},
-                {id: "run", default: 0x0000FF, name: "Run"},
-                {id: "sprint", default: 0xFFFF00, name: "Sprint"},
-                {id: "swim", default: 0xFFFF00, name: "Swim"},
-                {id: "fly", default: 0xFFFF00, name: "Fly"},
-                {id: "climb", default: 0xFFFF00, name: "Climb"},
-                {id: "stealth", default: 0xFF8000, name: "Stealth"},
+                // Some colors duplicated. I wanted to get this uploaded and rushed these out
+                {id: "crawl", default: 0xB45205, name: "Crawl"},
+                {id: "standard", default: 0x79F12F, name: "Standard"},
+                {id: "run", default: 0xF1DF2F, name: "Run"},
+                {id: "sprint", default: 0xF16D2F, name: "Sprint"},
+                {id: "swim", default: 0x2FE1F1, name: "Swim"},
+                {id: "fly", default: 0x2F86F1, name: "Fly"},
+                {id: "stealth", default: 0x702FF1, name: "Stealth"},
+                {id: "jumppack", default: 0xE62FF1, name: "Jump Pack"},
+                {id: "fulldefence", default: 0xB45205, name: "Full Defence"},
+                {id: "charge", default: 0xF16D2F, name: "Charge"},
+                {id: "fallback", default: 0xB45205, name: "Fall Back"},
+                {id: "climb", default: 0xB45205, name: "Climb"}
             ]
         }
-
         getRanges(token) {
-            const baseSpeed = token.actor.data.data.combat.speed
-            //const fly = actor.data.data.combat.fly
-            let ranges = []
-            ranges.push({range: baseSpeed / 2, color: "crawl"})
-            ranges.push({range: baseSpeed, color: "standard"})
-            ranges.push({range: baseSpeed * 2, color: "run"})
-            ranges.push({range: baseSpeed * 3, color: "sprint"})
-            console.log(ranges)
+            let landSpeed = token.actor.data.data.combat.speed
+            let flySpeed = token.actor.data.data.combat.fly
+            // Item list to check if Jump Pack is carried
+            let itemlist = []
+            for (let f of token.actor?.items) {
+                itemlist.push(f?.data.name)
+            }
+            // Check terrain environment under token for swimming
+            let terrain = canvas.terrain.terrainFromPixels(token.data.x, token.data.y) ?? [{environment: {id: ""}}]
+            // Range types
+            let crawl = {range: landSpeed / 2, color: "crawl"}
+            let standard = {range: landSpeed, color: "standard"}
+            let run = {range: landSpeed * 2, color: "run"}
+            let sprint = {range: landSpeed * 3, color: "sprint"}
+            // Assuming 'water' terrain environment is x2 movement cost.
+            let swim = {range: landSpeed, color: "swim"}
+            let fly = {range:  Math.max(landSpeed, flySpeed), color: "fly"}
+            let jumppack = {range: landSpeed * 2, color: "jumppack"}
+            // Stealth movement cost added in condition check
+            let stealth = {range: landSpeed / 2, color: "stealth"}
+            let fulldefence = {range: Math.max(landSpeed, flySpeed) / 2, color: "fulldefence"}
+            let charge = {range: landSpeed * 2, color: "charge"}
+            // Assuming a token can use its fly speed when climbing
+            let climb = {range: landSpeed / 2, color: "climb"}
+            let fallback = {range: landSpeed / 2, color: "fallback"}
+            // Default movement ranges, these are modified by conditions, terrain, elevation, and Jump Pack
+            let ranges = [
+                crawl,
+                standard,
+                run,
+                sprint,
+                fallback,
+                charge,
+                climb
+            ]
+            // If a token is "Dying", use the combat tracker to "Mark Defeated"
+            if (token.data?.overlayEffect === "icons/svg/skull.svg") {
+                ranges = [
+                    crawl,
+                    fallback
+                ]
+                return ranges
+            }
+            if (token.actor.hasCondition("exhausted")) {
+                ranges = [
+                    crawl,
+                    walk,
+                    fallback
+                ]
+                return ranges
+            }
+            if (token.actor.hasCondition("staggered")) {
+                ranges = [
+                    crawl,
+                    standard,
+                    fallback
+                ]
+                return ranges
+            }
+            if (token.actor.hasCondition("prone")) {
+                ranges = [
+                    crawl
+                ]
+                return ranges
+            }
+            /*If a token can and will be flying or using a Jump Pack then
+              set elevation with token HUD to +1 before starting movement*/
+            if (token.data.elevation > 0) {
+                if (flySpeed > 0) {
+                    ranges = [
+                        fly,
+                        fallback
+                    ]
+                } else {
+                    ranges = [
+                    ]
+                }
+                if (itemlist.indexOf("Jump Pack") >= 0) {
+                    ranges.push(jumppack)
+                    console.log("adding Jump Pack")
+                }
+                return ranges
+            }
+            if (terrain[0]?.environment.id === "water") {
+                ranges = [
+                    swim
+                ]
+                return ranges
+            }
+            // Assuming a token can crawl and climb while sneaking
+            if (token.data.hidden) {
+                ranges = [
+                    stealth,
+                    crawl,
+                    climb
+                ]
+                return ranges
+            }
+            if (token.actor.hasCondition("restrained")) {
+                ranges = [
+                ]
+                return ranges
+            }
+            if (token.actor.hasCondition("all-out-attack")) {
+                ranges = [
+                ]
+                return ranges
+            }
+            if (token.actor.hasCondition("full-defence")) {
+                ranges = [
+                    fulldefence
+                ]
+                return ranges
+            }
             return ranges
-            //if (fly > 0){
-            //    canmove.fly = true
-            //}
-
-
-            /*if (actor.hasCondition("prone")) {
-                const ranges = [
-                    {
-                        range: baseSpeed / 2, color: "crawl"
-                    },
-                    {
-                        range: baseSpeed, color: "standard"
-                    }
-                ]
-            }*/
-
-            /*if (actor.hasCondition("exhausted")) {
-                const ranges = [
-                    {
-                        range: baseSpeed / 2, color: "crawl"
-                    },
-                    {
-                        range: baseSpeed, color: "standard"
-                    }
-                ]
-            }*/
-
-            /*if (actor.hasCondition("restrained")) {
-                const ranges = [
-                    {
-                        range: 0, color: "crawl"
-                    }
-                ]
-            }*/
-            /*for (test in canmove) {
-                if()
-            }*/
-
-            /*const ranges = [
-                {range: speed, color: "walk"},
-                {range: baseSpeed * 2, color: "dash"}
-            ]*/
-
-
-            //return ranges
         }
     }
-
     dragRuler.registerModule("wng-dragruler", WnGSpeedProvider)
 })
